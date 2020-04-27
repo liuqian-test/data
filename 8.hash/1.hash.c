dbe2cd21fc13383737e7782962aa23d7c34b687a
liuqian-test <1254143911@qq.com>
2020-04-27 23:21:18 +0800

    hash

---
+++ 8.hash/1.hash.c
@@ -0,0 +1,98 @@
+/***************************************************************
+*       &Author: liuqian     
+*       &E-mail: *******.com
+*       &Motto: dudududu.
+*       &File Name: 1.hash.c
+*       &Created Time: 2020年04月27日 星期一 23时20分15秒
+*                      
+*       
+****************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Node {
+    char *str;
+    struct Node *next;
+} Node;
+
+typedef struct HashTable {
+    Node **data;
+    int size;
+} HashTable;
+
+Node *init_node(char *str, Node *head) {
+    Node *p = (Node *)malloc(sizeof(Node));
+    p->str = strdup(str);
+    p->next = head;
+    return p;
+}
+
+HashTable *init_hashtable(int n) {
+    HashTable *h = (HashTable *)malloc(sizeof(HashTable));
+    h->size = n << 1;
+    h->data = (Node **)calloc(h->size, sizeof(Node *));
+    return h;
+}
+
+int BKDRHash(char *str) {
+    int seed = 31, hash = 0;
+    for (int i = 0; str[i]; i++) hash = hash * seed + str[i];
+    return hash & 0x7fffffff;
+}
+
+int insert(HashTable *h, char *str) {
+    int hash = BKDRHash(str);
+    int ind = hash % h->size;
+    h->data[ind] = init_node(str, h->data[ind]);
+    return 1;
+}
+
+int search(HashTable *h, char *str) {
+    int hash = BKDRHash(str);
+    int ind = hash % h->size;
+    Node *p = h->data[ind];
+    while (p && strcmp(p->str, str)) p = p->next;
+    return p != NULL;
+}
+
+void clear_node(Node *node) {
+    if (node == NULL) return ;
+    Node *p = node, *q;
+    while (p) {
+        q = p->next;
+        free(p->str);
+        free(p);
+        p = q;
+    }
+    free(q);
+    return ;
+}
+
+void clear_hashtable(HashTable *h) {
+    if (h == NULL) return ;
+    for (int i = 0; i < h->size; i++) clear_node(h->data[i]);
+    free(h->data);
+    free(h);
+    return ;
+}
+
+int main() {
+    int op;
+    #define max_n 100
+    char str[max_n + 5];
+    HashTable *h = init_hashtable(max_n + 5);
+    while (~scanf("%d%s", &op, str)) {
+        switch (op) {
+            case 0:
+                printf("insert %s to hash table\n", str);
+                insert(h, str);
+                break;
+            case 1:
+                printf("search %s from hash table resule = %d\n", str, search(h, str));
+                break;
+        }
+    }
+    clear_hashtable(h);
+    return 0;
+}
